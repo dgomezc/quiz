fa8d4b946ae0185eea318fd600921afb978958a3
dgomez <davidpepi@hotmail.com>
2015-08-09 13:33:44 +0200

    Cambio de estructura visual de varias vistas para hacerlas más atractivas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -36,11 +36,9 @@ exports.show = function(req, res) {
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  var resultado = 'Incorrecto';
-  if (req.query.respuesta === req.quiz.respuesta) {
-      resultado = 'Correcto';
-  }
-  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: []});
+  var resultado = req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase();
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: req.query.respuesta,
+              esCorrecta: resultado, errors: []});
 };
 
 //GET /quizes/new
